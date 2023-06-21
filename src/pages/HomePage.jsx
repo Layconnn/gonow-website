9f790de50171ee7973afac86fb8d542171898817
khalil <olamilekanjinadu21@gmail.com>
2023-06-20 17:29:29 -0700

    adjusting home input

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ function Homepage(){
                 <p className="top__logo">GoNow</p>
                 <div className="top__navbar">
                     <p className="top__navbar__link" onClick={() => route('/')}>Home</p>
-                    <p className="top__navbar__linktwo">Create tasks</p>
+                    <p className="top__navbar__linktwo">Create task</p>
                     <p className="top__navbar__linkthree" onClick={() => route('/business')}>For Business</p>
                 </div>
                 <div className="top__GoNow">GoNow</div>
