1e75d0e2b87c35f98ec24de65d67fe46d0d3ed3f
khalil <olamilekanjinadu21@gmail.com>
2023-06-20 18:06:37 -0700

    adjusting home inputs

--- src/components/inputHome.jsx
+++ src/components/inputHome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from 'react';
-import '../styles/components/input-two.scss';
+import '../styles/components/input-home.scss';
 
 function InputHome(props){
     const [focused, setFocused] = useState(false)
